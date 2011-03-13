24ee06b3e7af38d6bf265eafb86340c1c5fc6150
HK Chai <hk.chai@gmail.com>
2011-03-13 16:44:35 +0800

    Removing redundant files

--- shutserver.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import socket
-s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM);
-# The setsockopt line is very important or else
-# python won't send to broadcast address
-HOST, PORT = '192.168.1.2', 9999;
-s.sendto("Data", (HOST,PORT));
-s.close();
